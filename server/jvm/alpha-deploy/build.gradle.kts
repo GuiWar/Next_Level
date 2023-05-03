643d00db5c8a77871a3f35999aa0cc1e37208a57
GuiWar <guilherme.guerra@genesis.global>
2023-05-03 13:03:09 +0000

    first commit

---
+++ server/jvm/alpha-deploy/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("global.genesis.deploy")
+}
+
+description = "alpha-deploy"
+
+dependencies {
+    genesisServer(
+        group = "global.genesis",
+        name = "genesis-distribution",
+        version = properties["genesisVersion"].toString(),
+        classifier = "bin",
+        ext = "zip"
+    )
+    genesisServer(
+        group = "global.genesis",
+        name = "auth-distribution",
+        version = properties["authVersion"].toString(),
+        classifier = "bin",
+        ext = "zip"
+    )
+
+    genesisServer(project(":alpha-distribution", "distribution"))
+    genesisServer(project(":alpha-site-specific", "distribution"))
+    genesisWeb("client:web")
+
+    api(project(":alpha-eventhandler"))
+    api(project(":alpha-messages"))
+    // Add additional dependencies on other external distributions here
+}
+tasks {
+    copyDependencies {
+        from(configurations.getByName("genesisServer"))
+    }
+}
