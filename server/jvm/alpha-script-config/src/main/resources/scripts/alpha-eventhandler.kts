fc8e9b4053ce6bb6ecf6b8b9238ddebb41ac9053
GuiWar <guilherme.guerra@genesis.global>
2023-05-03 17:25:27 +0000

    Go to the next level - State Machine

--- server/jvm/alpha-script-config/src/main/resources/scripts/alpha-eventhandler.kts
+++ server/jvm/alpha-script-config/src/main/resources/scripts/alpha-eventhandler.kts
@@ -1,19 +1,42 @@
-/**
- * System              : Genesis Business Library
- * Sub-System          : multi-pro-code-test Configuration
- * Version             : 1.0
- * Copyright           : (c) Genesis
- * Date                : 2022-03-18
- * Function : Provide event handler config for multi-pro-code-test.
- *
- * Modification History
- */
-eventHandler {
+import global.genesis.TradeStateMachine
 
-    eventHandler<Trade>(name = "TRADE_INSERT") {
-        schemaValidation = false;
+eventHandler {
+    val stateMachine = inject<TradeStateMachine>()
+
+    eventHandler<Trade>(name = "TRADE_INSERT", transactional = true) {
+        schemaValidation = false
         onCommit { event ->
-            entityDb.insert(event.details)
+            val trade = event.details
+            trade.enteredBy = event.userName
+            stateMachine.insert(trade)
+            ack()
+        }
+    }
+
+    eventHandler<TradeCancelled>(name = "TRADE_CANCELLED", transactional = true) {
+        onCommit { event ->
+            val message = event.details
+            stateMachine.modify(message.tradeId) { trade ->
+                trade.tradeStatus = TradeStatus.CANCELLED
+            }
+            ack()
+        }
+    }
+
+    eventHandler<TradeAllocated>(name = "TRADE_ALLOCATED", transactional = true) {
+        onCommit { event ->
+            val message = event.details
+            stateMachine.modify(message.tradeId) { trade ->
+                trade.tradeStatus = TradeStatus.ALLOCATED
+            }
+            ack()
+        }
+    }
+
+    eventHandler<Trade>(name = "TRADE_MODIFY", transactional = true) {
+        onCommit { event ->
+            val trade = event.details
+            stateMachine.modify(trade)
             ack()
         }
     }
