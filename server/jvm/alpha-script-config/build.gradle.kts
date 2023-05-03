d764724407308dfc19c78ef6c743e52101d042da
GuiWar <guilherme.guerra@genesis.global>
2023-05-03 14:09:35 +0000

    Go to the next level - Add some data

--- server/jvm/alpha-script-config/build.gradle.kts
+++ server/jvm/alpha-script-config/build.gradle.kts
@@ -6,6 +6,7 @@ dependencies {
     api("global.genesis:genesis-pal-streamer")
     api("global.genesis:genesis-pal-streamerclient")
     api("global.genesis:genesis-pal-eventhandler")
+    api("global.genesis:genesis-pal-consolidator")
     compileOnly(project(path = ":alpha-dictionary-cache", configuration = "codeGen"))
     testCompileOnly(project(":alpha-config"))
     testImplementation("global.genesis:genesis-dbtest")
