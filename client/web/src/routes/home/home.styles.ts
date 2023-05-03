b9e8d3261fd6ae7e04582bf927e0578f10d6e421
GuiWar <guilherme.guerra@genesis.global>
2023-05-03 15:11:44 +0000

    Go to the next level - Create a form to input data

--- client/web/src/routes/home/home.styles.ts
+++ client/web/src/routes/home/home.styles.ts
@@ -1,6 +1,31 @@
 import { css } from '@microsoft/fast-element';
+import { mixinScreen } from '../../styles';
 
 export const HomeStyles = css`
   :host {
+    ${mixinScreen('flex')}
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+
+  .column-split-layout {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    width: 50%;
+  }
+
+  .row-split-layout {
+    justify-content: center;
+    display: flex;
+    flex-direction: row;
+    flex: 1;
+    width: 100%;
+    height: 50%;
+  }
+
+  zero-text-field {
+  display: block;
   }
 `;
--- client/web/src/routes/home/home.ts
+++ client/web/src/routes/home/home.ts
@@ -1,6 +1,7 @@
-import { customElement, FASTElement } from '@microsoft/fast-element';
+import { customElement, FASTElement, observable } from '@microsoft/fast-element';
 import { HomeTemplate as template } from './home.template';
 import { HomeStyles as styles } from './home.styles';
+import {Connect} from '@genesislcap/foundation-comms';
 
 const name = 'home-route';
 
@@ -10,10 +11,39 @@ const name = 'home-route';
   styles,
 })
 export class Home extends FASTElement {
+    @observable public quantity: string;
+    @observable public price: string;
+    @observable public instrument: string;
+    @observable public side: string = 'BUY';
+
   constructor() {
     super();
   }
+   @Connect connect: Connect;
 
+   public async insertTrade() {
+     const insertTradeRequest = await this.connect.commitEvent('EVENT_TRADE_INSERT', {
+       DETAILS: {
+         COUNTERPARTY_ID: 'GENESIS',
+         INSTRUMENT_ID: this.instrument,
+         QUANTITY: this.quantity,
+         PRICE: this.price,
+         SIDE: this.side,
+         TRADE_DATETIME: Date.now(),
+       },
+       IGNORE_WARNINGS: true,
+       VALIDATE: false,
+     });
+   }
+
+    @observable tradeInstruments: Array<{value: string, label: string}>;
+    public async connectedCallback() {
+        super.connectedCallback();
+
+        const tradeInstrumentsRequest = await this.connect.request('INSTRUMENT');
+        this.tradeInstruments = tradeInstrumentsRequest.REPLY?.map(instrument => ({value: instrument.INSTRUMENT_ID, label: instrument.INSTRUMENT_ID}));
+        this.instrument = this.tradeInstruments[0].value;
+    }
     public singlePositionActionColDef = {
       headerName: 'Action',
       minWidth: 120,
